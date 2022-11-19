06f4d291848fa7cb9b7cbb45ac5c4299e09f92f0
JerryInterestelar <57443693+JerryInterestelar@users.noreply.github.com>
2022-11-19 14:29:55 -0300

    Algumas alterações

--- Sistemas Operacionais/Problema do papai noel/papainoel.c
+++ Sistemas Operacionais/Problema do papai noel/papainoel.c
@@ -3,6 +3,8 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <syscall.h>
 
 #define true 1
 #define false 0
@@ -19,84 +21,93 @@ sem elfoTex;
 sem multex;
 
 
-int wait(sem *semaforo){
+int wait(sem *semaforo, char alguem[], char onde[]){
+    printf("%s quer fazer ateracao em %s\n", alguem, onde);
     return sem_wait(semaforo);
 }
-int signal(sem *semaforo, int parametro){
-    return sem_post(semaforo);    
+int signal(sem *semaforo, int parametro, char alguem[], char onde[]){
+    printf("%s liberou o caminho em %s\n", alguem, onde);
+    for (int i = 0; i < parametro; i++) sem_post(semaforo);
+    return 0; 
 }
 
-void prepararTreno(){
-    printf("Preparando treno.....\n");
-    usleep(500);
+void prepararTreno(int id){
+    printf("[THREAD Papai Noel %d] Preparando treno.....\n", id);
+    sleep(500);
 }
-void ajudarElfos(){
-    printf("Ajudando elfos.....\n");
-    usleep(500);
+void ajudarElfos(int id){
+    printf("[THREAD Papai Noel %d] Ajudando elfos.....\n", id);
+    sleep(500);
 }
-void serAmarrada(){
-    printf("Amarrando....\n");
-    usleep(500);
+void serAmarrada(int id, int renas_c){
+    printf("[THREAD Rena %d, id:%d] Amarrando....\n", renas_c, id);
+    sleep(500);
 }
-void pedirAjuda(){
-    printf("Pedindo ajuda...\n");
-    usleep(1000);
+void pedirAjuda(int id, int elfo_c){
+    printf("[THREAD Elfo %d, id:%d] Pedindo ajuda...\n",elfo_c, id);
+    sleep(500);
 }
 
 void* papaiNoelThread(void *arg){
     while (true){
-        wait(&papaiNoelSem);
-        wait(&multex);
+        int *threadId = (int *) arg;
+
+        wait(&papaiNoelSem, "Papai noel", "Papai Noel SEM");
+        wait(&multex, "Papai Noel", "Multex");
         if (renas >= 9){
-            prepararTreno();
+            prepararTreno(*threadId);
 
             /* No codigo do livro, o signal recebe 9 como
              * parametro, nao sei pq.
              * Vou deixar na funcao para fins futuros
              * */
-            signal(&renaSem, 9); 
+            signal(&renaSem, 9, "Papai Noel", "Rena SEM"); 
             renas -= 9;
-        }else if (elfos == 3) ajudarElfos();
-        signal(&multex, 0);
+        }else if (elfos == 3) ajudarElfos(*threadId), signal(&elfoTex, 1, "Papai Noel", "Elfo TEX"), elfos -=3 ;
+        signal(&multex, 1, "Papai Noel", "Multex");
     }
 }
 
 void* renaThread(void *arg){
     while (true){
-        wait(&multex);
+        int *threadId = (int *) arg;
+        wait(&multex, "Rena", "Multex");
         renas++;
-        if (renas == 9) signal(&papaiNoelSem, 0);
-        signal(&multex, 0);
+        if (renas == 9) signal(&papaiNoelSem, 1, "Rena", "Papai Noel SEM");
+        signal(&multex, 1, "Rena", "Multe");
 
-        wait(&renaSem);
-        serAmarrada();
+        wait(&renaSem, "Rena", "Rena SEM");
+        serAmarrada(*threadId, renas);
     }
 }
 
 
 void* elfoThread(void *arg){
     while (true){
-        wait(&elfoTex);
-        wait(&multex);
+        int *threadInd = (int *)arg;
+
+
+        wait(&elfoTex, "Elfo", "Elfo TEX");
+        wait(&multex, "Elfo", "Multex");
         elfos++;
-        if (elfos == 3) signal(&papaiNoelSem, 0);
-        else signal(&elfoTex, 0);
+        if (elfos == 3) signal(&papaiNoelSem, 1, "Elfo", "Papai Noel SEM");
+        else signal(&elfoTex, 1, "Elfo", "Elfo TEX");
 
-        signal(&multex, 0);
+        signal(&multex, 1, "Elfo", "Multex");
 
-        pedirAjuda();
+        pedirAjuda(*threadInd, elfos);
 
-        wait(&multex);
+        wait(&multex, "Elfo", "Multex");
         elfos--;
-        if (elfos == 0) signal(&elfoTex, 0);
-        signal(&multex, 0);
+        if (elfos == 0) signal(&elfoTex, 1, "Elfo", "Elfo TEX");
+        signal(&multex, 1, "Elfo", "Multex");
     }
 }
 
 
 // Declaração de algumas constantes e apelidos
 
-#define NELFOS 50
+#define NELFOS 10
 #define NRENAS 9
 
 typedef pthread_t pthread;
@@ -111,20 +122,24 @@ int main(){
      *
      * */
 
+    pthread papaiNoel;
+    pthread elfo[NELFOS];
+    pthread rena[NRENAS];
 
+    puts("Inicializando semaforos");
     sem_init(&papaiNoelSem, 0, 1);
     sem_init(&renaSem, 0, 1);
     sem_init(&elfoTex, 0, 1);
     sem_init(&multex, 0, 1);
 
-    pthread papaiNoel;
-    pthread elfo[NELFOS];
-    pthread rena[NRENAS];
 
-    pthread_create(&papaiNoel, NULL, papaiNoelThread, NULL);
-    for (int i = 0; i < NELFOS; i++) pthread_create(&elfo[i], NULL, elfoThread, NULL);
+    puts("Criando as Threads");
+    puts("Em execução....");
 
-    for (int i = 0; i < NRENAS; i++) pthread_create(&rena[i], NULL, renaThread, NULL);
+    pthread_create(&papaiNoel, NULL, papaiNoelThread, (void *)&papaiNoel);
+    for (int i = 0; i < NELFOS; i++) pthread_create(&elfo[i], NULL, elfoThread, (void *)&elfo[i]);
+
+    for (int i = 0; i < NRENAS; i++) pthread_create(&rena[i], NULL, renaThread, (void *)&rena[i]);
 
 
     pthread_join(papaiNoel, NULL);
@@ -132,6 +147,7 @@ int main(){
 
     for (int i = 0; i < NRENAS; i++) pthread_join(rena[i], NULL);
     
+
     sem_destroy(&papaiNoelSem);
     sem_destroy(&renaSem);
     sem_destroy(&elfoTex);
