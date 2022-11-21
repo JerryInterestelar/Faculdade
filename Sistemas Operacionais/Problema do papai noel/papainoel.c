de73ee7b454ff25935511f7fb15093cf69e9186c
Jerry <57443693+JerryInterestelar@users.noreply.github.com>
2022-11-21 12:46:18 +0000

    Correção de comentários

--- Sistemas Operacionais/Problema do papai noel/papainoel.c
+++ Sistemas Operacionais/Problema do papai noel/papainoel.c
@@ -47,70 +47,70 @@ void pedirAjuda(int id){
     sleep(1);
 }
 
-void* papaiNoelThread(void *arg){                               // Thread do papai noel
-    while (true){                                               // Ele executara esta rotina para sempre
+void* papaiNoelThread(void *arg){               // Thread do papai noel
+    while (true){                               // Ele executara esta rotina para sempre
         int *threadId = (int *) arg;                            
 
-        wait(&papaiNoelSem);    // Espera até que um elfo ou rena mande um sinal para ele
-        wait(&multex);                  // Espera até conseguir acesso à região critica
+        wait(&papaiNoelSem);                    // Espera até que um elfo ou rena mande um sinal para ele
+        wait(&multex);                          // Espera até conseguir acesso à região critica
                                                                 
-        if (renas == 9){                                        // Se tiver nove renas esperando 
-            prepararTreno(*threadId);                           // Papai noel prepara o treno
-            signal(&renaSem, 9);      // E sinaliza o semaforo das renas nove vezes
-            renas = 0;                                          // E zera o numero de renas esperando 
+        if (renas == 9){                        // Se tiver nove renas esperando 
+            prepararTreno(*threadId);           // Papai noel prepara o treno
+            signal(&renaSem, 9);                // E sinaliza o semaforo das renas nove vezes
+            renas = 0;                          // E zera o numero de renas esperando 
         }else if (elfos == 3) {
-                                                                // Se tiver tres elfos esperando
-            ajudarElfos(*threadId);                             // Papai noel irá ajuda-los
-            signal(&elfoSem, 3);      // E sinaliza que mais tres elfos podem ser ficar esperando o atendimento
+                                                // Se tiver tres elfos esperando
+            ajudarElfos(*threadId);             // Papai noel irá ajuda-los
+            signal(&elfoSem, 3);                // E sinaliza que mais tres elfos podem ser ficar esperando o atendimento
         }
-        signal(&multex, 1);             // Por fim libera a região critica para outra thread altera-la
+        signal(&multex, 1);                     // Por fim libera a região critica para outra thread altera-la
     }
 }
 
-void* renaThread(void *arg){                                    // Thread da Rena
-                                                                // Como a rena so vai ser amarrada uma vez, essa thread nao precisa
-                                                                // estar um um loop infinito
+void* renaThread(void *arg){                    // Thread da Rena
+                                                // Como a rena so vai ser amarrada uma vez, essa thread nao precisa
+                                                // estar um um loop infinito
 
     int *threadId = (int *) arg;
 
-    wait(&multex);                            // Espera para conseguir acesso a regiao critica
-    renas++;                                                    // incrementam o buffer de contagem de renas
-    if (renas == 9)                                             // Quando esta for a nona rena
-        signal(&papaiNoelSem, 1);     // Sinaliza ao papai noel que todas as renas estao prontas para serem amarradas
-    signal(&multex, 1);                        // Libera o acesso a regiao critica
+    wait(&multex);                              // Espera para conseguir acesso a regiao critica
+    renas++;                                    // incrementam o buffer de contagem de renas
+    if (renas == 9)                             // Quando esta for a nona rena
+        signal(&papaiNoelSem, 1);               // Sinaliza ao papai noel que todas as renas estao prontas para serem amarradas
+    signal(&multex, 1);                         // Libera o acesso a regiao critica
 
-    wait(&renaSem);                         // Agora, todas as renas aguardam o sinal para serem amarradas pelo papai noel
-    serAmarrada(*threadId);                                     // Quando liberadas pelo papai noel, estas são amarradas
+    wait(&renaSem);                             // Agora, todas as renas aguardam o sinal para serem amarradas pelo papai noel
+    serAmarrada(*threadId);                     // Quando liberadas pelo papai noel, estas são amarradas
 
     return 0;
 }
 
 
-void* elfoThread(void *arg){                                    // Thread da rena
-    while (true){                                               // Repita para sempre
+void* elfoThread(void *arg){                    // Thread da rena
+    while (true){                               // Repita para sempre
         int *threadId = (int *)arg;
 
 
-        wait(&elfoTex);                     // Espera para conseguir acesso a fila de atendimento do papai noel
-        wait(&multex);                        // Espera para conseguir acesso a regia critica
-        elfos++;                                                // incrementa o buffer da fila "elfos"
-        if (elfos == 3)                                         // Se ouver tres elfos na fila
-            signal(&papaiNoelSem, 1); // É sinalizado ao papai noel que ele precisa atender os elfos
-                                                                // e a fila de espera trava ate que estes sejam atendidos pela thread
-                                                                // do papai noel
+        wait(&elfoTex);                         // Espera para conseguir acesso a fila de atendimento do papai noel
+        wait(&multex);                          // Espera para conseguir acesso a regia critica
+        elfos++;                                // incrementa o buffer da fila "elfos"
+        if (elfos == 3)                         // Se ouver tres elfos na fila
+            signal(&papaiNoelSem, 1);           // É sinalizado ao papai noel que ele precisa atender os elfos
+                                                // e a fila de espera trava ate que estes sejam atendidos pela thread
+                                                // do papai noel
         else 
-            signal(&elfoTex, 1);            // Libera o acesso a fila para que mais um elfo possa entrar
+            signal(&elfoTex, 1);                // Libera o acesso a fila para que mais um elfo possa entrar
 
-        signal(&multex, 1);                   // Libera o acesso a regiao critica
+        signal(&multex, 1);                     // Libera o acesso a regiao critica
 
-        wait(&elfoSem);                     // Apenas o elfos que estiverem na fila poderam esperar pelo o atendimento do papai noel
-        pedirAjuda(*threadId);                                  // Procedimento de pedir ajuda
+        wait(&elfoSem);                         // Apenas o elfos que estiverem na fila poderam esperar pelo o atendimento do papai noel
+        pedirAjuda(*threadId);                  // Procedimento de pedir ajuda
 
-        wait(&multex);                        // Pede acesso a regiao critica mais uma vez para liberar o seu espaco no buffer de fila "elfos"
+        wait(&multex);                          // Pede acesso a regiao critica mais uma vez para liberar o seu espaco no buffer de fila "elfos"
         elfos--;
-        if (elfos == 0)                                         // Quando nao tiver mais nenhum elfo na fila 
-            signal(&elfoTex, 1);            // O ultimo elfo sinaliza que mais elfos podem entrar
-        signal(&multex, 1);                   // Libera o acesso a regia critica
+        if (elfos == 0)                         // Quando nao tiver mais nenhum elfo na fila 
+            signal(&elfoTex, 1);                // O ultimo elfo sinaliza que mais elfos podem entrar
+        signal(&multex, 1);                     // Libera o acesso a regia critica
 
     }
 }
@@ -131,7 +131,7 @@ pthread rena[NRENAS];
 int main(){
 
     /*
-     * Compilação com linkers
+     * Compilação e execução com linkers
      * gcc papainoel.c -lpthread -lrt -D_REENTRANT -lpthread -o teste; ./teste
      *
      * */
